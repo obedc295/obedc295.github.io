87796f95b01cb0775d37c29bbca3a9ea7beae8e1
Obed Castellanos <163482058+obedc295@users.noreply.github.com>
2024-04-17 18:04:57 -0600

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-import netlify from "@astrojs/netlify";
-
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://obedc295.github.io',
   integrations: [tailwind()],
-  output: "server",
-  adapter: netlify()
+  output: "server"
 });
